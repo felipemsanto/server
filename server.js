f5749aa93171a8dee371a206a036ad6f524db3d0
Felipe dos Santos <felipemirandajd13@hotmail.com>
2020-07-01 17:41:42 -0300

    Template Engine is already working

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express')
+const nunjucks = require('nunjucks');
+const { static } = require('express');
+
 const server = express()
 
+server.use(express.static('public'))
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express: server
+})
+
 server.get("/", function(req, res) {
-    return res.send("Hi!!")
+    return res.render("about")
+})
+
+server.get("/classes", function(req, res) {
+    return res.render("classes")
 })
 
 server.listen(5000, function() {
