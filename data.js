5eb7c0f18ff6ce99e6cbdd7e4d7c55966a99200e
Felipe dos Santos <felipemirandajd13@hotmail.com>
2020-07-02 10:54:06 -0300

    Data script with video values

---
+++ data.js
@@ -0,0 +1,45 @@
+module.exports = [
+    {
+        id: "thwgWgPLec4",
+        title: "Polinômios - 3ª série EM",
+        duration: "33 min",
+        price: "Free"
+
+    },
+    {
+        id: "CKdeR8P5SqY",
+        title: "Funções - 1ª série EM",
+        duration: "33 min",
+        price: "Free"
+
+    },
+    {
+        id: "6etAnfyPJKU",
+        title: "Matriz - 2ª série EM",
+        duration: "28 min",
+        price: "Free"
+
+    },
+    {
+        id: "tZASsDi_TCo",
+        title: "Operações com Polinômios - 3ª série",
+        duration: "32 min",
+        price: "Free"
+
+    },
+    {
+        id: "HeuXuZd8gv4",
+        title: "Matrizes e Determinantes  - 2ª série EM",
+        duration: "33 min",
+        price: "Free"
+
+    },
+    {
+        id: "O5uIlMUG9qY",
+        title: "Matrizes e Determinantes (Parte 2) - 2ª série EM",
+        duration: "8 min",
+        price: "Free"
+
+    }
+    
+]
