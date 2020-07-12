688de6779c7df4237d7d0be836d9d9d598cfd7f3
Felipe dos Santos <felipemirandajd13@hotmail.com>
2020-07-12 11:39:55 -0300

    correct video tumblrs

--- data.js
+++ data.js
@@ -2,6 +2,7 @@ module.exports = [
     {
         id: "thwgWgPLec4",
         title: "Polinômios - 3ª série EM",
+        image: "assets/img/tumblr_polinomios.PNG",
         duration: "33 min",
         price: "Free"
 
@@ -9,6 +10,7 @@ module.exports = [
     {
         id: "CKdeR8P5SqY",
         title: "Funções - 1ª série EM",
+        image: "assets/img/tumblr_funcoes.PNG",
         duration: "33 min",
         price: "Free"
 
@@ -16,6 +18,7 @@ module.exports = [
     {
         id: "6etAnfyPJKU",
         title: "Matriz - 2ª série EM",
+        image: "assets/img/tumblr_matriz.PNG",
         duration: "28 min",
         price: "Free"
 
@@ -23,6 +26,7 @@ module.exports = [
     {
         id: "tZASsDi_TCo",
         title: "Operações com Polinômios - 3ª série",
+        image: "assets/img/tumblr_operacoes_polinomios.PNG",
         duration: "32 min",
         price: "Free"
 
@@ -30,6 +34,7 @@ module.exports = [
     {
         id: "HeuXuZd8gv4",
         title: "Matrizes e Determinantes  - 2ª série EM",
+        image: "assets/img/tumblr_matriz_determinantes.PNG",
         duration: "33 min",
         price: "Free"
 
@@ -37,6 +42,7 @@ module.exports = [
     {
         id: "O5uIlMUG9qY",
         title: "Matrizes e Determinantes (Parte 2) - 2ª série EM",
+        image: "assets/img/tumblr_matriz_determinantes_pt2.PNG",
         duration: "8 min",
         price: "Free"
 
